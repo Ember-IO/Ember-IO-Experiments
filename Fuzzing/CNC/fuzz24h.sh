2e1f9a58344877918b8e5963e7f2776b1424e265
Ember-IO <guyfdow2@hotmail.com>
2023-05-11 12:59:44 +0930

    Fix typo

--- Fuzzing/CNC/fuzz24h.sh
+++ Fuzzing/CNC/fuzz24h.sh
@@ -9,4 +9,4 @@ if [[ $# -eq 0 ]]; then
 else
   out_dir=$1
 fi
-$EMBER_BASE_DIR/AFLplusplus/afl-fuzz -V 3600 -i ./seeds -o $out_dir -t 200 -Q ./CNC.elf -sram_size 192k -flash_size 2048k -sram2_base 0x10000000 -sram2_size 64k -machine embedded_fuzz -fuzz_input @@
+$EMBER_BASE_DIR/AFLplusplus/afl-fuzz -V 86400 -i ./seeds -o $out_dir -t 200 -Q ./CNC.elf -sram_size 192k -flash_size 2048k -sram2_base 0x10000000 -sram2_size 64k -machine embedded_fuzz -fuzz_input @@
